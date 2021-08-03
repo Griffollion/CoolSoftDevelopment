28587f6679363fe828c2ea4a316e70cf65685c53
Dmitry0403 <85955541+Dmitry0403@users.noreply.github.com>
2021-08-03 19:09:22 +0300

    поменял код по созданию кнопок

--- src/app.js
+++ src/app.js
@@ -2,17 +2,25 @@ import styles from './css/styles.css'
 import { Button } from './components/buttons/button'
 import { NewCard } from './components/NewCard/NewCard'
 
-const newCard = new NewCard()
+const newCard = new NewCard('.container-global')
 newCard.render()
 
-function buttonCreator (icon, className, name) {
-  const button = new Button(icon, className, name)
-  button.render()
-}
-buttonCreator('icn__btnuser', '.user', 'Участники')
-buttonCreator('icn__btnaccess_time', '.data', 'Дата')
-buttonCreator('icn__btnarrow-right2', '.moving', 'Перемещение')
-buttonCreator('icn__btncontent_copy', '.copying', 'Копирование')
-buttonCreator('icn__btnvideo_label', '.archiving', 'Архивация')
-buttonCreator('', '.card-info__actions-btn', 'Показать подробности')
-buttonCreator('', '.card-info__description-btn', 'Изменить')
+// function buttonCreator (icon, node, name) {
+//   const button = new Button(icon, node, name)
+//   button.render()
+// }
+// buttonCreator('icn__btnuser', '.user', 'Участники')
+const usersButton = new Button('icn__btnuser', '.user', 'Участники')
+usersButton.render()
+const dateButton = new Button('icn__btnaccess_time', '.data', 'Дата')
+dateButton.render()
+const movButton = new Button('icn__btnarrow-right2', '.moving', 'Перемещение')
+movButton.render()
+const copyButton = new Button('icn__btncontent_copy', '.copying', 'Копирование')
+copyButton.render()
+const delButton = new Button('icn__btnvideo_label', '.archiving', 'Архивация')
+delButton.render()
+const showButton = new Button('', '.card-info__actions-btn', 'Показать подробности')
+showButton.render()
+const changeButton = new Button('', '.card-info__description-btn', 'Изменить')
+changeButton.render()
