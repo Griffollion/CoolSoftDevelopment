033ca547a609af567221dba8880c4710d1a5bcbc
AlexKarseka <alexkarseko@gmail.com>
2021-08-03 16:36:00 +0300

    Alex_Karseka-Card-part2

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
 import styles from './css/styles.css'
 import { Button } from './components/buttons/button'
+import { NewCard } from './components/NewCard/NewCard'
 
-// const button = new Button('.container-global','название кнопки')
-// button.render()
+const newCard = new NewCard()
+newCard.render()
+
+function buttonCreator (icon, className, name) {
+  const button = new Button(icon, className, name)
+  button.render()
+}
+buttonCreator('icn__btnuser', '.user', 'Участники')
+buttonCreator('icn__btnaccess_time', '.data', 'Дата')
+buttonCreator('icn__btnarrow-right2', '.moving', 'Перемещение')
+buttonCreator('icn__btncontent_copy', '.copying', 'Копирование')
+buttonCreator('icn__btnvideo_label', '.archiving', 'Архивация')
+buttonCreator('', '.card-info__actions-btn', 'Показать подробности')
+buttonCreator('', '.card-info__description-btn', 'Изменить')
--- src/components/buttons/button.js
+++ src/components/buttons/button.js
@@ -1,16 +1,13 @@
 import styles from './button.css'
 
-export function Button (node, text) {
+export function Button (icon, node, text) {
   this.template = document.createElement('div')
-  this.template.className = 'button'
+  this.template.className = `button ${icon}`
   this.template.textContent = text
   this.render = function () {
     const container = document.querySelector(node)
     container.append(this.template)
-    
   }
-  
   // this.template.onclick = function () {
   //  }
 }
-
