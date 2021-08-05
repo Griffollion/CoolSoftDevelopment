18e765d19d617aeffbc895f63d86dd84afad1f7f
Dmitry0403 <85955541+Dmitry0403@users.noreply.github.com>
2021-08-05 14:37:04 +0300

    добавил главные карточки на страницу

--- src/components/buttons/button.js
+++ src/components/buttons/button.js
@@ -5,9 +5,10 @@ export function Button (icon, node, text) {
   this.template.className = `button ${icon}`
   this.template.textContent = text
   this.render = function () {
-    const container = document.body
+    const container = document.querySelector(node)
     container.append(this.template)
   }
-  // this.template.onclick = function () {
-  //  }
+//   this.template.onclick = function () {
+   
+//    }
 }
