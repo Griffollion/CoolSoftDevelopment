1a4c21e7bb2c6bb3f379b6372e204e5f553ff6ad
Dmitry0403 <85955541+Dmitry0403@users.noreply.github.com>
2021-08-09 13:32:24 +0300

    поменял структуру файлов

---
+++ src/initUserInterface.js
@@ -0,0 +1,26 @@
+export function addArea() {
+  if (document.querySelector(".card_task")) {
+  } else {
+    const input = document.createElement("div");
+    input.innerHTML = `<textarea class="card_task"></textarea>`;
+    let container = document.querySelector(".textarea");
+    return container.append(input);
+  }
+}
+
+export function addTask(className, text) {
+  let node = document.createElement("div");
+  node.className = `task ${className}`;
+  node.innerHTML = text;
+  let container = document.querySelector(".card_list-tasks");
+  return container.append(node);
+}
+
+export function addTasksToCardsFromStorage() {
+  let tasksArr = JSON.parse(localStorage.getItem("tasksArray"));
+  if (tasksArr) {
+    tasksArr.forEach((element) => {
+      addTask(element.class, element.title);
+    });
+  }
+}
