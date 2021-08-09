e61c47db9ec0673e1cc1050361aff069dbd1b35f
Roman <rekun.olya@gmail.com>
2021-08-09 07:28:33 +0300

    fixing

--- src/components/components.js
+++ src/components/components.js
@@ -1,12 +1,18 @@
+  
 export function addArea() {
+  if (document.querySelector(".card_task")){
+  } else {
   const input = document.createElement("div");
   input.innerHTML = `<textarea class="card_task"></textarea>`;
   let container = document.querySelector(".textarea");
   return container.append(input);
+  }
 }
 
-export function addTask(tag) {
-  tag.className = "task in_done";
+export function addTask(className, text) {
+  let node = document.createElement("div");
+  node.className = `task ${className}`;
+  node.innerHTML = text;
   let container = document.querySelector(".card_list-tasks");
-  return container.append(tag);
+  return container.append(node);
 }
