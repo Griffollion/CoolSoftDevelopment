5b6c20d8ded38c1c94f505327dc5a724bc3e5af6
Dmitry0403 <85955541+Dmitry0403@users.noreply.github.com>
2021-08-10 00:09:05 +0300

    написал функцию выбора задания по клику и поиск его данных в localStorage

--- src/components/NewCard/NewCard.js
+++ src/components/NewCard/NewCard.js
@@ -1,8 +1,9 @@
 import styles from './NewCard.css'
 import { NewCardTemplate } from './NewCardTemplate'
 
-export function NewCard (node) {
+export function NewCard (node, obj) {
   this.component = NewCardTemplate.content.querySelector('div').cloneNode(true)
+  this.component.querySelector('.new__card-header-name').innerHTML = obj.title
   this.render = function () {
     const body = document.querySelector(node)
     body.append(this.component)
