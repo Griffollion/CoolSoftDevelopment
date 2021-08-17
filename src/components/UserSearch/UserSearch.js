ed8f94e866c34b3dba75747c81ed743d57550f46
Dmitry0403 <85955541+Dmitry0403@users.noreply.github.com>
2021-08-17 22:53:28 +0300

    пофиксил баг

--- src/components/UserSearch/UserSearch.js
+++ src/components/UserSearch/UserSearch.js
@@ -44,7 +44,7 @@ export function ToCloseModalUsersTemplate() {
 }
 
 export function catchUser(obj) {
-  let usersList = document.querySelector(".card-info__container-users-icons")
+  let usersList = document.querySelector(".card-info__container-users-icons");
   let allUsers = document.querySelectorAll(".user-search__form--user-list");
   allUsers.forEach((el) => {
     el.addEventListener("click", (event) => {
@@ -55,14 +55,15 @@ export function catchUser(obj) {
         if (ele.id === obj.id) {
           if (!ele["users"]) {
             ele["users"] = [userName];
+          } else {
+            ele["users"].push(userName);
           }
-          ele["users"].push(userName);
         }
-        let user = document.createElement("div")
-        user.innerHTML = userName
-        user.className = ("classUser")
-        usersList.append(user)
       });
+      let user = document.createElement("div");
+      user.innerHTML = userName;
+      user.className = "classUser";
+      usersList.append(user);
       localStorage.setItem("tasksArray", JSON.stringify(getKey));
     });
   });
